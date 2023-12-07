ce15c3cb49b2103f941e54e0364aaa7398e4251f
Biruk Zewdie <biruk.tegegn@gmail.com>
2023-12-07 01:36:07 -0500

    adding javascript file for random color

---
+++ app.js
@@ -0,0 +1,18 @@
+function randomColor (){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+const letters = document.querySelectorAll('.letter');
+
+
+
+setInterval(function(){
+    for (let letter of letters){
+        letter.style.color = randomColor ();
+    }
+
+
+}, 1000)
